34acea5d1c1131711932ae5ea5a6cee159ffdc44
Suraj Kothari <surajvkothari@gmail.com>
2019-12-06 20:10:54 +0000

    Completed Day 1

--- Day1.py
+++ Day1.py
@@ -1,1 +1,12 @@
-print("hello")
+def calcFuel(m):
+    return int(m / 3) - 2
+
+data = []
+with open("data.txt") as f:
+    for line in f:
+        data.append(int(line.strip("\n")))
+
+#print(data)
+
+fuel = [calcFuel(i) for i in data]
+print(sum(fuel))
