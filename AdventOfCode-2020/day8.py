4b66fc29ade32dc866453cb1cd18768445155aac
Suraj Kothari <surajvkothari@gmail.com>
2021-01-01 20:58:53 +0000

    Completed part 2!

--- AdventOfCode-2020/day8.py
+++ AdventOfCode-2020/day8.py
@@ -4,9 +4,11 @@
 """ Part 1 """
 
 # Gets instructions
-#instruction_set = []
-with open("day8_data.txt") as f:
-    instruction_set = [i.strip().split() for i in f]
+def get_instructions():
+    with open("day8_data.txt") as f:
+        instruction_set = [i.strip().split() for i in f]
+
+    return instruction_set
 
 def parse_instructions(instruction_set):
     ACCUMULATOR = 0
@@ -14,25 +16,50 @@ def parse_instructions(instruction_set):
     instructions_visited = []
 
     while True:
-        instruction = instruction_set[instruction_pointer]
-        if instruction_pointer in instructions_visited:
-            print("Acumulator:",ACCUMULATOR)
-            break # Exit, otherwise infinite loop will occur
+        if instruction_pointer < len(instruction_set):
+            instruction = instruction_set[instruction_pointer]
+            if instruction_pointer in instructions_visited:
+                return (False, ACCUMULATOR) # Return false to indicate infinite loop
+            else:
+                instructions_visited.append(instruction_pointer)
+
+                opcode = instruction[0]
+                operand = int(instruction[1])
+
+                if opcode == "acc":
+                    # Increase or decrease accumulator
+                    ACCUMULATOR += operand
+                    instruction_pointer += 1
+                elif opcode == "jmp":
+                    # Change instruction pointer
+                    instruction_pointer += operand
+                elif opcode == "nop":
+                    # Do nothing
+                    instruction_pointer += 1
         else:
-            instructions_visited.append(instruction_pointer)
+            return (True, ACCUMULATOR) # Return true to indicate succesful ending
 
-            opcode = instruction[0]
-            operand = int(instruction[1])
 
-            if opcode == "acc":
-                # Increase or decrease accumulator
-                ACCUMULATOR += operand
-                instruction_pointer += 1
-            elif opcode == "jmp":
-                # Change instruction pointer
-                instruction_pointer += operand
-            elif opcode == "nop":
-                # Do nothing
-                instruction_pointer += 1
+instruction_set = get_instructions()
+ended, acc = parse_instructions(instruction_set)
+print("Accumulator (Part 1):", acc)
 
-parse_instructions(instruction_set)
+""" Part 2 """
+ended = False
+instruction_change_pointer = 0
+while not ended:
+    instruction_set_copy = get_instructions() # Make a copy of the original instruction set
+
+    operand = instruction_set_copy[instruction_change_pointer][0]
+
+    if operand == "nop":
+        instruction_set_copy[instruction_change_pointer][0] = "jmp"
+    elif operand == "jmp":
+        instruction_set_copy[instruction_change_pointer][0] = "nop"
+
+
+    ended, acc = parse_instructions(instruction_set_copy) # Will return true if ended
+    instruction_change_pointer += 1
+
+
+print("Accumulator (Part 2):", acc)
