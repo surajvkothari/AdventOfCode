a5c3b3c781b99cc7cfd43fd45cd926b81ec9cb2d
Suraj Kothari <surajvkothari@gmail.com>
2021-01-01 22:21:56 +0000

    Completed part 2 of day 9

--- AdventOfCode-2020/day9.py
+++ AdventOfCode-2020/day9.py
@@ -8,13 +8,14 @@ def get_data():
 
     return data
 
+
 def check_sum(nums, target):
     for i in nums:
         for j in nums:
             if i != j and (i + j == target):
                 return True
 
-    print(target)
+    print("Failed num (part 1):", target)
     return False
 
 
@@ -28,3 +29,21 @@ while reached_target:
     prev_nums = nums[pos-preamble:pos]
     reached_target = check_sum(nums=prev_nums, target=target)
     pos += 1
+
+
+""" Part 2 """
+invalid_number = 105950735
+
+for n in range(2, 100):
+    slice_n = n
+    end_slice = nums.index(invalid_number) - slice_n
+
+    for i in range(end_slice+1):
+        nums_sliced = nums[i:i+slice_n]
+        sum_slice = sum(nums_sliced)
+
+        if sum_slice == invalid_number:
+            print("Encryption weakness (part 2):",min(nums_sliced) + max(nums_sliced))
+            quit() # Stop program
+        elif sum_slice > invalid_number:
+            break
