147e9ece0b6b10b86abfc9839beb9bdbf01abb89
Suraj Kothari <surajvkothari@gmail.com>
2023-12-21 16:04:35 +0000

    Started AoC 2023

---
+++ 2023/day_14.py
@@ -0,0 +1,19 @@
+# Advent of code 2023
+# Day 14
+
+def get_data():
+    with open("day_14_data.txt") as f:
+        pass
+
+    return None
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+data = get_data()
+part1()
