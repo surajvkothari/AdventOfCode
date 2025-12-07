b03b9f2c414a4d43710f1d808a576639e7ea4bfe
Suraj Kothari <surajvkothari@gmail.com>
2025-12-07 15:30:24 +0000

    Day 2 - Part 2

--- 2015/day_2.py
+++ 2015/day_2.py
@@ -13,11 +13,18 @@ def get_data():
     return dimensions
 
 
-def formula(l, w, h):
+def wrapping_formula(l, w, h):
     # Slack is the area of the smallest side
     slack = min([l*w, w*h, l*h])
 
     return 2*l*w + 2*w*h + 2*h*l + slack
+
+def ribbon_formula(l, w, h):
+    # Ribbon is the smallest face's perimeter
+    ribbon = min([2*l+2*w, 2*w+2*h, 2*l+2*h])
+    bow = l*w*h  # Cubic volume
+
+    return ribbon + bow
 
 
 def part1(data):
@@ -25,10 +32,21 @@ def part1(data):
     
     for dimension in data:
         # Unpack dimension when passing to formula
-        total += formula(*dimension)
+        total += wrapping_formula(*dimension)
 
     print("PART 1:", total)
 
 
+def part2(data):
+    total = 0
+    
+    for dimension in data:
+        # Unpack dimension when passing to formula
+        total += ribbon_formula(*dimension)
+
+    print("PART 2:", total)
+
+
 data = get_data()
 part1(data)
+part2(data)
