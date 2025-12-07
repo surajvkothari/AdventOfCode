5ce8d261c10ad365df639bf2b17d2bda61fb1249
Suraj Kothari <surajvkothari@gmail.com>
2025-12-07 15:09:01 +0000

    Day 1 - Part 2

--- 2015/day_1.py
+++ 2015/day_1.py
@@ -10,6 +10,7 @@ def get_data():
 
 def part1(data):
     floor = 0
+    
     for char in data:
         if char == '(':
             floor += 1
@@ -19,5 +20,21 @@ def part1(data):
     print("PART 1:", floor)
 
 
+def part2(data):
+    floor = 0
+    
+    for index, char in enumerate(data, 1):
+        if char == '(':
+            floor += 1
+        elif char == ')':
+            floor -= 1
+        
+        # Check if floor is basement
+        if floor == -1:
+            print("PART 2:", index)
+            return None
+
+
 data = get_data()
 part1(data)
+part2(data)
