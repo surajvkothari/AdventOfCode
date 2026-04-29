4a7ca0e12d24accbfb03d4d16912a60295fde64f
Suraj Kothari <surajvkothari@gmail.com>
2026-04-29 15:52:52 +0100

    Day 1 - Part 2

--- 2017/day_1.py
+++ 2017/day_1.py
@@ -11,17 +11,27 @@ def part1(seq):
     digit_matches = []
 
     for i in range(len(seq)):
-        if i == (len(seq)-1):
-            # List is circular, so wrap around to the beginning when i is at the end
-            if seq[i] == seq[0]:
-                digit_matches.append(seq[i])
-        else:
-            # Check if the next digit matches
-            if seq[i] == seq[i+1]:
-                digit_matches.append(seq[i])
+        # List is circular, so wrap around to the beginning when i is at the end
+        if seq[i] == seq[ (i+1) % len(seq) ]:
+            digit_matches.append(seq[i])
 
     print("Part 1:", sum(digit_matches))
 
+
+def part2(seq):
+    seq = [int(x) for x in seq]
+    digit_matches = []
+
+    for i in range(len(seq)):
+        # List is circular, so wrap around to the beginning when i is at the end
+        # Compare to the digit halfway around the circular list
+        if seq[i] == seq[ (i+len(seq)//2) % len(seq) ]:
+            digit_matches.append(seq[i])
+
+    print("Part 2:", sum(digit_matches))
+
+
 seq = get_data()
 part1(seq)
+part2(seq)
 
